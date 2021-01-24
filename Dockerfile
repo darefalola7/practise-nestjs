e6ce0118deb2a9437bfb690bb6322c109ed5ba2c
dare_falola <darefalola7@gmail.com>
2021-01-24 18:37:24 +0100

    added auth module

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:alpine
+FROM node:14.15.4-alpine3.10
 
 ENV CI=true
 
@@ -6,7 +6,20 @@ WORKDIR /app
 
 COPY package.json ./
 
-#RUN yarn install
+#COPY yarn.lock ./
+#
+#RUN yarn --registry http://registry.npmjs.org
+#
+#RUN yarn --version
+#
+#RUN yarn cache clean --force
+
+RUN npm config set proxy null
+RUN npm config set https-proxy null
+RUN npm config set registry http://registry.npmjs.org/
+
+RUN npm install --verbose
+#RUN npm install
 #--production=true
 #RUN yarn add global @nestjs/cli
 
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtPayloadInterface } from './jwt-payload.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_KEY,
+    });
+  }
+
+  //whatever is returned from here will be injected to the request object
+  async validate(payload: JwtPayloadInterface) {
+    const { username } = payload;
+    const user = await this.userRepository.findOne({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
